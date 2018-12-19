5c52b056ef03c37f7d6b75da59d1fdfd6e8100cf
dowpm <mcdowpm@gmail.com>
2018-12-19 12:38:54 -0500

    copy cli, environment.rb, Gemfile from develop branch

--- Gemfile
+++ Gemfile
@@ -7,3 +7,6 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 gem 'sqlite3'
 gem 'pry'
+gem 'rake'
+gem 'require_all'
+gem 'rspec'
---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'bundler'
+Bundler.require
+
+DB = {:conn => SQLite3::Database.new("db/contacts.db")}#// the connection of our database
+
+# DB[:conn].execute("DROP TABLE IF EXISTS contacts") #// drop contacts to avoid error
+
+require_all 'lib'
