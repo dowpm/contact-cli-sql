b4d8750c116839f17cf5e337a83799d443fd10bc
dowpm <mcdowpm@gmail.com>
2018-12-19 12:06:26 -0500

    add require_all gem and require all files in lib folder in environment file

--- Gemfile
+++ Gemfile
@@ -8,3 +8,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem 'sqlite3'
 gem 'pry'
 gem 'rake'
+gem 'require_all'
--- config/environment.rb
+++ config/environment.rb
@@ -5,4 +5,4 @@ DB = {:conn => SQLite3::Database.new("db/contacts.db")}#// the connection of our
 
 # DB[:conn].execute("DROP TABLE IF EXISTS contacts") #// drop contacts to avoid error
 
-require_relative '../lib/contact.rb'
+require_all 'lib'
