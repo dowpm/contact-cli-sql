7aea4c6fdfddd7002a30a1dab48dba27ab5cec6a
dowpm <mcdowpm@gmail.com>
2018-12-20 00:12:58 -0500

    add exit to show_option

--- lib/application.rb
+++ lib/application.rb
@@ -23,14 +23,16 @@ class Application
     end
 
     def self.show_option
-        #----------------------------------Menu
+        #----------------------------------Menu show
         table = Terminal::Table.new :title => "Menu", :rows => CHOICES
         table.style = {
             :all_separators => true,:padding_left => 3, 
             :border_x => "=", :border_i => "x"
         }
         puts table
+
         option = gets.strip.to_i
+
         while option <= 0 || option > 7
             system "clear" or system "cls"
             puts "Option out of range", "Please choose an option:"
@@ -38,7 +40,20 @@ class Application
             option = gets.strip.to_i
         end
         #-------------------
-        
+
+        case option
+        when 1
+            system "clear" or system "cls"
+            add_contact
+        when 0 , 7
+            puts "Do you really want to exit (Y/N)"
+            exi = gets.strip.downcase
+            if exi == "n" or exi == "no"
+                return "start"
+            end
+            puts "Byeeeeeee"
+            "exit"
+        end
         
     end
 end
