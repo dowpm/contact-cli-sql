ab2c72915d4be62dd9e96abdca2ac9eeb5187e33
dowpm <mcdowpm@gmail.com>
2018-12-20 15:28:25 -0500

    everything works

--- lib/application.rb
+++ lib/application.rb
@@ -59,7 +59,7 @@ class Application
             system "clear" or system "cls"
             add_contact
         when 2
-            return 'show_option' if check_for_contact? "see"
+            return 'show_option' if check_for_contact? "show"
             puts "What number of contacts do you want to see? (1-10) "
             print "=> "
             from_number = gets.strip.to_i
@@ -67,32 +67,41 @@ class Application
             from_number = Contact.all.size if from_number > Contact.all.size
             #print_contacts
             print_contacts from_number
-            puts "\n","Choose an option:"
-            "show_option"
+            puts "Hit enter to go to the menu"
+            gets.strip
+            system "clear" or system "cls"
+            # puts "\n","Choose an option:"
+            "start"
         when 3
             return 'show_option' if check_for_contact? "find"
-            puts "Enter the firstname"
+            puts "** Hit !q to go back to the menu **","Enter the firstname"
             print "=> "
-            firstname = gets.strip.capitalize
+            firstname = gets.strip
+            system 'cls' or system 'clear'; return "start" if firstname.downcase == "!q"
+            firstname = firstname.capitalize
             if !Contact.find_by_firstname(firstname).empty? 
                 print_contacts_find firstname
-                puts "\n"
-                "show_option"
+                puts "Hit enter to go to the menu"
+                gets.strip
+                system "clear" or system "cls"
+                "start"
             else
-                puts 'Sorry!!! No contact was found'
+                puts 'Sorry!!! No contact was found',"\n"
+                puts "Choose an option"
                 "show_option"
             end
         when 4
-            check_for_contact? "edite"
+            return 'show_option' if check_for_contact? "edit"
             edit_contact
         when 5
-            check_for_contact? "delete"
+            return 'show_option' if check_for_contact? "delete"
             delete_contact
         when 6
-            check_for_contact? "delete"
+            return 'show_option' if check_for_contact? "delete"
             puts "Do you really want to delete all the contacts? (y/n)"
             print "=> "
             del = gets.strip.downcase
+            system 'cls' or system 'clear'; return "start" if del == "!q"
             if del == 'y' or del == 'yes'
                 Contact.delete_all_contact
                 system 'cls' or system 'clear'
@@ -100,12 +109,13 @@ class Application
                 "start"
             else
                 system 'cls' or system 'clear'
-                "show_option"
+                "start"
             end
         when 7
             puts "Do you really want to exit? (y/n)"
             exi = gets.strip.downcase
             if exi == "n" or exi == "no"
+                system 'cls' or system 'clear'
                 return "start"
             end
             puts "Byeeeeeee"
@@ -149,8 +159,8 @@ class Application
             return "add_contact" 
         else
             contact = Contact.create_contact info
-            puts "#{contact.firstname} #{contact.lastname} has been successfully added to your contact\n"
-            puts "Now you have #{Contact.all.size} contact(s)","Choose an option:","\n"
+            puts "#{contact.firstname} #{contact.lastname} has been successfully added to your contact","\n"
+            puts "Now you have #{Contact.all.size} contact(s)","\n" ,"Choose an option:"
             "show_option"
         end
     end
@@ -167,6 +177,7 @@ class Application
 
         index = gets.strip
         if index.upcase == "!Q"
+            system 'cls' or system 'clear'
             return "start"
         end
 
@@ -214,13 +225,13 @@ class Application
             info.unshift contact.id
             contact = Contact.create_contact info
             puts "#{contact.firstname} #{contact.lastname} has been successfully edited \n"
-            puts "Choose an option:","\n"
+            puts "\n"
             "show_option"
         end        
     end
 
     def self.delete_contact
-        puts "** Type !q to back to the menu **","Choose the contact to delete:"
+        puts "** Type !q to back to the menu **","\n","Choose the contact to delete:"
         contacts = Contact.all.each.with_index(1).map do |person, index|
             "#{index}. #{person.firstname} #{person.lastname}"
         end.join("\n")
@@ -228,6 +239,7 @@ class Application
         print "=> "
         index = gets.strip
         if index.upcase == "!Q"
+            system 'cls' or system 'clear'
             return "start"
         end
         index = index.to_i
@@ -245,8 +257,8 @@ class Application
             contact = Contact.all[index-1]
             contact.delete
             puts "#{contact.firstname} #{contact.lastname} has been deleted","\n"
-            puts "Choose an option:"
-            "show_option"
+            # puts "Choose an option:"
+            "start"
         end
     end
 
@@ -284,4 +296,4 @@ class Application
     end
 end
 
-binding.pry
+# binding.pry
