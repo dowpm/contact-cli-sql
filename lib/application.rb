924648ecf0771b4354f039c63f19e500b27b688f
dowpm <mcdowpm@gmail.com>
2018-12-19 23:48:49 -0500

    refactor menu section on show_option method

--- lib/application.rb
+++ lib/application.rb
@@ -7,9 +7,9 @@ class Application
         "Enter the new email","Enter the new address",
         "Enter the new phone number","Enter the new profession"]
     CHOICES = [
-        "1. Add a contact", "2. Show contacts asc", 
-        "3.Find contact by firstname" "4. Edit a contact", 
-        "5. Delete a contact", "6. Delete all contacts", "7. Exit to My_contact", "\n"]
+        ["1. Add a contact", "2. Show contacts asc" ],
+        ["3.Find contact by firstname", "4. Edit a contact"], 
+        ["5. Delete a contact", "6. Delete all contacts"], ["7. Exit to My_contact", "\n"]]
 
     def self.welcome
         puts "\t\t--------- Welcome to My_Contact ---------","\n"
@@ -21,4 +21,26 @@ class Application
         puts "-----------------------------------------------","Choose an option:"
         "show_option"
     end
+
+    def self.show_option
+        #----------------------------------Menu
+        table = Terminal::Table.new :title => "Menu", :rows => CHOICES
+        table.style = {
+            :all_separators => true,:padding_left => 3, 
+            :border_x => "=", :border_i => "x"
+        }
+        puts table
+        option = gets.strip.to_i
+        while option <= 0 || option > 7
+            system "clear" or system "cls"
+            puts "Option out of range", "Please choose an option:"
+            puts table
+            option = gets.strip.to_i
+        end
+        #-------------------
+        
+        
+    end
 end
+
+binding.pry
